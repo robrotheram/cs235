ed690805874ca7bb0f78df0846a2ea9f394c0f6d
Kerry <lez_vamp@hotmail.com>
2013-02-20 12:42:02 +0000

    OK so changed this back to an abstract class to enforce a constructor to set variables needed to make charts in the way Rob did in his examples. Chart class signatures will need to both implement visualisation and extend chart e.g. public class MS_BarChart extends chart implements visualisation {} ... Unfinished atm =)

--- cs235/src/cs235/MS_Chart.java
+++ cs235/src/cs235/MS_Chart.java
@@ -4,15 +4,84 @@
  */
 package cs235;
 
+import java.awt.BorderLayout;
+import java.awt.Rectangle;
+import java.util.Date;
+import javax.swing.JPanel;
+import org.jfree.chart.ChartPanel;
+
 /**
  *
  * @author vampie
  */
-public interface MS_Chart {
+public abstract class MS_Chart extends JPanel {
+    private MS_DataSet M_DB;
     
-    boolean getDataSet(int[] dataSet);
-    boolean getDataSet(double[] dataSet);
-    boolean getDataSet(String[] dataSet);
+    private final int X_AXISDATAPOSITION;
+    private final int Y_AXISDATAPOSITION;
+    private final String TITLE;
     
+    public MS_Chart(MS_DataSet db,int xColumnPos, int yColPos, String title, Rectangle r){
+        M_DB = db;
+       X_AXISDATAPOSITION = xColumnPos;
+       Y_AXISDATAPOSITION = yColPos;
+       TITLE = title;
+
+       this.setBounds(r);
+       this.setLayout(new java.awt.BorderLayout());
+       this.add(createPanel(),BorderLayout.CENTER);
+       this.setVisible(true);
+    };
     
+    boolean setTimeDate(Date currentDate){
+    
+        return true;
+    };
+    
+    boolean setAuthor(String description){
+        
+        return true;
+    };
+    
+    boolean setDescription(String description){
+        return true;
+    };
+    
+    boolean setChartTitle(String chartTitle){
+        return true;
+    };
+    
+    boolean setHeight(int height){
+        return true;
+    };
+    
+    boolean setWidth(int width){
+        return true;
+    };
+    
+    boolean setPostion(int x, int y){
+        return true;
+    };
+    
+    boolean convertDataSet(int[] dataSet){
+        return true;
+    };
+    
+    boolean convertDataSet(double[] dataSet){
+        return true;
+    };
+    
+    boolean convertDataSet(String[] dataSet){
+        return true;
+    };
+    
+    boolean createChart(){
+        return true;
+    };
+    
+    private ChartPanel createPanel(){
+        ChartPanel myChart = new ChartPanel(createChart());
+           myChart.setMouseWheelEnabled(true);
+        return myChart;
+    }
 }
