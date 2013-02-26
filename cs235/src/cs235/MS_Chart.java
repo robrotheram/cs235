3d2b61b4153eb1b5972db5045a8708e73c2b299e
Kerry <lez_vamp@hotmail.com>
2013-02-26 21:01:23 +0000

    Doxygen comments added. Most methods don't have any parameters or return anything as they are unimplemented abstract methods and for classes that do not take any parameters or return anything you leave the @param or @return from the comments.

--- cs235/src/cs235/MS_Chart.java
+++ cs235/src/cs235/MS_Chart.java
@@ -14,14 +14,26 @@ import org.jfree.chart.JFreeChart;
 /**
  *
  * @author Kerryanne Tolhurst
+ * The chart class is the abstract super class for all charts.
+ * It sets the constructor for all classes and sets all methods needed to be
+ * implemented by all subclasses.
+ * 
  */
 public abstract class MS_Chart extends JPanel {
-    private MS_DataSet M_DB;
     
+    private MS_DataSet M_DB;
     private final int X_AXISDATAPOSITION;
     private final int Y_AXISDATAPOSITION;
     private final String TITLE;
     
+    /**
+     * constructor setting all class variables needed to create a chart
+     * @param db - the data for the chart
+     * @param xColumnPos - the column data for the x axis from the dataset
+     * @param yColPos - the column data for the y axis from the dataset
+     * @param title - chart title
+     * @param r - to display the chart in the windows native size
+     */
     public MS_Chart(MS_DataSet db,int xColumnPos, int yColPos, String title, Rectangle r){
        M_DB = db;
        X_AXISDATAPOSITION = xColumnPos;
@@ -34,16 +46,37 @@ public abstract class MS_Chart extends JPanel {
        this.setVisible(true);
     };
     
+    /**
+     * Abstract class for setting the chart title. 
+     */
     abstract boolean setChartTitle();
     
+    /**
+     * Abstract class for converting the program dataset to the format needed to
+     * make a chart from it.
+     */
     abstract boolean convertDataSet();
     
+    /**
+     * Abstract class theat returns the array of the current colour map 
+     */
     abstract boolean getColourMap();
     
+    /**
+     * Abstract class that sets the current colour map and carries out any 
+     * processing to change the colour of the chart elements
+     */
     abstract boolean setColourMap();
     
+    /**
+     * Abstract class that creates the actual chart 
+     */
     abstract public JFreeChart createChart();
     
+    /**
+     * Creates a chart panel from the chart
+     * @return myChart 
+     */
     private ChartPanel createPanel(){
         ChartPanel myChart = new ChartPanel(createChart());
            myChart.setMouseWheelEnabled(true);
