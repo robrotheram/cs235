ba3948f6a1295d8aa54f21f36fb09ccc929591ee
Wyler <guxiaoxi.mas@gmail.com>
2013-02-26 12:09:13 +0000

    interface for default color usercolor and colormap. These classes should have "implements MS_ColourScheme" in signature e.g class MS_DefaultColour implements MS_ ColourScheme

---
+++ cs235/src/cs235/MS_ColorScheme.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cs235;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author Wyler
+ */
+interface MS_ColorScheme {
+    
+     Color[] getColorArray();
+     Color getColor();
+     int getNumberOfColors();
+     
+    
+}
