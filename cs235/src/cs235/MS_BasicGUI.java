a3fedfac2fd45504822398e23dbc1f2ef1e09f31
Kerry <lez_vamp@hotmail.com>
2013-02-27 14:53:08 +0000

    implemented some methods for accessing and setting private variables

--- cs235/src/cs235/MS_BasicGUI.java
+++ cs235/src/cs235/MS_BasicGUI.java
@@ -8,6 +8,7 @@ import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -19,56 +20,72 @@ import javax.swing.JTabbedPane;
  * Creates a basic GUI 
  */
 public class MS_BasicGUI extends JFrame{
-    JPanel toolPanel, displayArea, optionPane;
-    JFrame chartOptions;
-    JTabbedPane displayTabs;
-    JButton importData, createChart, saveButton;
-    Container container;
+    private MS_DataSet m_db;
+    JPanel m_toolPanel, m_displayArea, m_optionPane;
+    JFrame m_chartOptions;
+    JTabbedPane m_displayTabs;
+    JButton m_importData, m_createChart, m_saveButton;
+    Container m_container;
     
     MS_BasicGUI(){
+        //create empty dataset
+        m_db = new MS_DataSet();
+        
         //create container for GUI
-        container = getContentPane();
-        container.setLayout(new BorderLayout());
+        m_container = getContentPane();
+        m_container.setLayout(new BorderLayout());
         
         //create display area for data and charts
-        displayArea = new JPanel();
-        displayTabs = new JTabbedPane();
-        displayArea.add(displayTabs);
+        m_displayArea = new JPanel();
+        m_displayTabs = new JTabbedPane();
+        m_displayArea.add(m_displayTabs);
         
         //create area for tools
-        toolPanel = new JPanel();
+        m_toolPanel = new JPanel();
         
         //create import button
-        importData = new JButton("Import");
+        m_importData = new JButton("Import");
         
         //create create chart button
-        createChart = new JButton("Create Chart");
+        m_createChart = new JButton("Create Chart");
         
         //add buttons to the tool panel
-        toolPanel.add(importData);
-        toolPanel.add(createChart);
+        m_toolPanel.add(m_importData);
+        m_toolPanel.add(m_createChart);
         
         //add components to the main container
-        container.add(displayArea);
-        container.add(toolPanel);
+        m_container.add(m_displayArea);
+        m_container.add(m_toolPanel);
         
         //create chart option window
-        chartOptions = new JFrame();
-        optionPane = new JPanel();
-        chartOptions.add(optionPane);
+        m_chartOptions = new JFrame();
+        m_optionPane = new JPanel();
+        m_chartOptions.add(m_optionPane);
         
         //add handlers 
-        MS_GUIHandler handler = new MS_GUIHandler();
-        importData.addActionListener(handler);
-        createChart.addActionListener(handler);
+        MS_GUIHandler m_handler = new MS_GUIHandler();
+        m_importData.addActionListener(m_handler);
+        m_createChart.addActionListener(m_handler);
         
     }
     
     public class MS_GUIHandler implements ActionListener {
-    public void actionPerformed(ActionEvent event) {
-        if(event.getSource() == importData){
-            
+        @Override
+        public void actionPerformed(ActionEvent event) {
+            if(event.getSource() == m_importData){
+                JFrame getFile = new MS_CsvFileDialog(m_db);
+                getFile.setVisible(true);
+                System.out.println("button pressed");
+            }else if(event.getSource() == m_createChart){
+                m_chartOptions.setVisible(true);
+            }
         }
     }
-}
+    
+    public static void main(String[] args) throws IOException {
+ 
+       MS_BasicGUI example1 = new MS_BasicGUI();
+       example1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       example1.setVisible(true);
+    }
 }
