4558910d3e1117bd16a05536bad8bd5a262f5f5b
Kerry <lez_vamp@hotmail.com>
2013-02-28 23:11:24 +0000

    Corrected spelling mistakes, doxygen comments, and coding conventions plus added a couple of methods I needed. Try commit the piece of crap!

--- cs235/src/cs235/MS_ColourMap.java
+++ cs235/src/cs235/MS_ColourMap.java
@@ -1,73 +1,109 @@
 package cs235;
 
 import java.awt.Color;
+import java.awt.Dimension;
+import javax.swing.JPanel;
 
 /**
  * 
  * @author Zhenjie Mu
  */
 public class MS_ColourMap implements MS_ColorScheme{
-    private Color[] colorarray = new Color[5];
+    private Color[] m_colorArray = new Color[5];
+    private JPanel m_colourPanel, m_defaultC2
+            , m_defaultC3, m_defaultC4, m_defaultC5;
+    private JPanel[] panels = {m_colourPanel, m_defaultC2
+            , m_defaultC3, m_defaultC4, m_defaultC5};
             
    /**
-    * constructor takes from color array
-    * @param colorarray 
+    * Constructor sets the color array for constructor
+    * 
+    * @param colorArray 
     */
-    public MS_ColourMap(Color[] colorarray){
-        this.colorarray = colorarray;
+    public MS_ColourMap(Color[] colorArray){
+        if(setColourArray(colorArray)){
+            System.out.println("Colours set in the colour map in MS_ColourMap");
+        } else if(!setColourArray(colorArray)){
+            System.err.println("Unable to set colours in MS_ColourMap");
+        }
+        if(setPanels(panels)){
+            System.out.println("Colour panels set in MS_ColourMap");
+        }
+        else if(!setPanels(panels)){
+            System.err.println("Colour panels not set in MS_ColourMap");
+        }
     }
     
- /**
- * 
- * The setColourArray class stores structure of entire color data
- * 
- * @param carray 
- */
-    
-        @Override
-    public void setColourArray(Color[] carray) {
-        colorarray = carray;
+    /**
+    * 
+    * The setColourArray class stores structure of entire color data
+    * 
+    * @param cArray 
+    */
+    @Override
+    public boolean setColourArray(Color[] cArray) {
+        m_colorArray = cArray;
+        return true;
     }
 
-     /**
-     * set in the color array a Color c at position i
+    /**
+    * set in the color array a Color c at position i
+    * 
+    * @param c the colour
+    * @param i the position
+    */
+    @Override
+    public boolean setColour(Color c, int i) {
+        m_colorArray[i] = c;
+        return true;
+    }
+    
+    /**
+     * Sets 5 panels sizes and colours to match the array for viewing colour
+     * map colours in an interface
      * 
-     * @param color
-     * @param int  
+     * @param panels array of panels
+     * @return true if set correctly
+     */
+    public boolean setPanels(JPanel[] panels){
+        for(int i = 0; i < panels.length; i++){
+            panels[i].setPreferredSize(new Dimension(30,30));
+            panels[i].setBackground(m_colorArray[i]);
+        }
+        return true;
+    }
+    
+    public JPanel[] getPanels(){
+        return panels;
+    }
+    
+    /**
+     * Get the colour array
+     * @return m_colorArray
      */
     @Override
-    public void setColour(Color c, int i) {
-        colorarray[i] = c;
-    
-    }
-    
-/**
- * get color from color array
- * @return color array
- */
-    @Override
     public Color[] getColourArray() {
-        return colorarray;
+        return m_colorArray;
     }
 
-     /**
-     * get individual color from color array
-     * @param int
+    /**
+    * Get individual colour from colour array
+    * @param i an int for the location of colour in array
+    * @return m_colorArray[i] the colour at position a
+    */
+    @Override
+    public Color getColour(int i) {
+        return m_colorArray[i];
+    }
+    
+    /**
+     * Returns the number of colours stored in array for calculations etc
+     * 
      * @return int
      */
     @Override
-    public Color getColour(int i) {
-        return colorarray[i];
-    }
-    
-/**
- * returns the number of color stored in array
- * 
- * @return int
- */
-    @Override
     public int getNumberOfColours() {
-        return colorarray.length;
+        return m_colorArray.length;
     }
     
 }
