b239cdee998f7b2e25c0122782ef016951cd6829
Kerry <lez_vamp@hotmail.com>
2013-02-28 21:09:41 +0000

    Partially implemented. Is bright red and yellow currently so panels were easy to see for alignment testing =P

--- cs235/src/cs235/MS_BasicGUI.java
+++ cs235/src/cs235/MS_BasicGUI.java
@@ -29,17 +29,29 @@ import javax.swing.JTextField;
  */
 public class MS_BasicGUI extends JFrame{
     private MS_DataSet m_db;
-    JPanel m_toolPanel, m_displayArea, m_optionPane;
-    JFrame m_chartOptions;
-    JTabbedPane m_displayTabs;
-    JButton m_importData, m_createChart, m_saveButton;
-    JComboBox m_xAxisData, m_yAxisData;
-    JTextField m_chartTitle;
-    Container m_container = getContentPane();;
-    double m_height, m_width;
-    int m_percentageHeight, m_percentageWidth;
-    Rectangle r = new Rectangle();
+    private JPanel m_toolPanel, m_displayArea, m_optionPane, m_defaultC1, m_defaultC2
+            , m_defaultC3, m_defaultC4, m_defaultC5, m_colour1, m_colour2, m_colour3,
+            m_colour4, m_colour5;
+    private JPanel[] defaultColourPanels = {m_defaultC2
+            , m_defaultC3, m_defaultC4, m_defaultC5};
+    private JPanel[] colourMap1Panels = {m_colour1, m_colour2, m_colour3,
+            m_colour4, m_colour5};
+    private JFrame m_chartOptions;
+    private JTabbedPane m_displayTabs;
+    private JButton m_importData, m_createChart, m_saveButton;
+    private JComboBox m_xAxisData, m_yAxisData;
+    private JTextField m_chartTitle;
+    private Container m_container = getContentPane();;
+    private double m_height, m_width;
+    private int m_percentageHeight, m_percentageWidth;
+    private Rectangle r = new Rectangle();
     private MS_BasicGUI m_Context;
+    private Color[] m_defaultColours = {new Color(2,89,89), new Color(0,175,181), 
+        new Color(189,51,103), new Color(242,206,22), new Color(204,135,4)};
+    private Color[] m_colourMap1 = {new Color(104,186,45), new Color(0,145,63), 
+        new Color(0,147,142), new Color(0,147,221), new Color(204,135,4)};
+    private MS_ColourMap m_defaultColour;
+    private MS_ColourMap m_colourMaps;
     
     public void MS_BasicGUI(){
         //create empty dataset
@@ -51,22 +63,17 @@ public class MS_BasicGUI extends JFrame{
         Toolkit m_kit = m_container.getToolkit(); 
         //saves the dimension of users screen size
         Dimension m_screenSize = m_kit.getScreenSize();
-        System.out.println(m_screenSize);
         m_container.setPreferredSize(m_screenSize);
         m_container.setLayout(new BoxLayout(m_container, BoxLayout.LINE_AXIS));
         m_height = m_screenSize.getHeight();
         m_width = m_screenSize.getWidth();
-        System.out.println(m_width + " " + m_height);
-        
         
         //create display area for data and charts
         m_percentageHeight = (int)(Math.round(m_height * 0.90));
         m_percentageWidth = (int) (Math.round(m_width * 0.70));
-        System.out.println(m_percentageWidth);
         m_displayArea = new JPanel();
         m_displayArea.setBackground(Color.yellow);
         m_displayArea.setPreferredSize(new Dimension(m_percentageWidth, m_percentageHeight));
-        System.out.println(m_displayArea.getHeight() + " " +  m_displayArea.getWidth());
         m_displayTabs = new JTabbedPane();
         m_displayTabs.setPreferredSize(new Dimension(m_percentageWidth, m_percentageHeight));
         m_displayArea.add(m_displayTabs);
@@ -94,15 +101,6 @@ public class MS_BasicGUI extends JFrame{
         m_container.add(m_displayArea);
         m_container.add(m_toolPanel);
         
-        //create chart option window
-        m_chartOptions = new JFrame();
-        m_optionPane = new JPanel();
-        m_chartTitle = new JTextField();
-        m_xAxisData = new JComboBox();
-        m_yAxisData = new JComboBox();
-        
-        m_chartOptions.add(m_optionPane);
-        
         //add handlers 
         MS_GUIHandler m_handler = new MS_GUIHandler();
         m_importData.addActionListener(m_handler);
@@ -112,6 +110,41 @@ public class MS_BasicGUI extends JFrame{
         setLocationRelativeTo(null);
         setVisible(true);
         
+    
+    }
+    
+    private void initOptionPane() {
+        //create chart option window components
+        m_chartOptions = new JFrame();
+        m_chartOptions.setSize(500,400);
+        m_optionPane = new JPanel();
+        m_optionPane.setPreferredSize(new Dimension(500,400));
+        m_chartTitle = new JTextField();
+        m_xAxisData = new JComboBox();
+        m_yAxisData = new JComboBox();
+        m_defaultColour = new MS_ColourMap(m_defaultColours);
+        m_colourMaps = new MS_ColourMap(m_colourMap1);
+        //populate combo boxes with values from the table data
+        String[] cols = m_db.getColumnNames();
+        for(int i =0; i<cols.length; i++){
+            m_xAxisData.addItem(cols[i]);
+            m_yAxisData.addItem(cols[i]);
+        }
+        
+        
+        //add the components to the window
+        m_chartOptions.add(m_optionPane);
+        pack();
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+    
+    private JPanel[] setColourPanels(Color[] colours, JPanel[] panels){
+        for (int i = 0; i < colours.length; i++){
+            panels[i].setPreferredSize(new Dimension(30,30));
+            panels[i].setBackground(colours[i]);
+        }
+        return panels;
     }
     
     public class MS_GUIHandler implements ActionListener {
