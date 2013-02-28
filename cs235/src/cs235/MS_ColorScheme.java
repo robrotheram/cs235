7887218d2cf4a01403737f2b58a468546f34f9ad
Kerry <lez_vamp@hotmail.com>
2013-02-28 21:38:45 +0000

    Changed the set methods to booleans to match bob's coding conventions and added doxygen comments

--- cs235/src/cs235/MS_ColorScheme.java
+++ cs235/src/cs235/MS_ColorScheme.java
@@ -7,14 +7,43 @@ package cs235;
 import java.awt.Color;
 
 /**
- *
+ * Interface implemented by subclasses to ensure they implement the needed methods
  * @author Wyler
  */
 interface MS_ColorScheme {
-    void setColourArray(Color[] carray);
-    void setColour(Color c , int i);
+    
+    /**
+     * 
+     * @param cArray an array of colours
+     * @return true if the array was set correctly
+     */
+    boolean setColourArray(Color[] cArray);
+    
+    /**
+     * 
+     * @param c colour to be set
+     * @param i position in the array to set it
+     * @return returns true if set
+     */
+    boolean setColour(Color c , int i);
+    
+    /**
+     * Allows access to the private colour array
+     * @return an array of colour
+     */
     Color[] getColourArray();
+    
+    /**
+     * Get's a single colour from the array
+     * @param i place of colour in the array
+     * @return the colour at the requested position
+     */
     Color getColour(int i);
+    
+    /**
+     * Get's the number of colours in the array for calculations etc.
+     * @return 
+     */
     int getNumberOfColours();
      
     
