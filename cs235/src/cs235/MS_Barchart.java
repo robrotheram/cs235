6a709233b759bc385eb9921613eedaada7b05fd3
William Bray <661857@swansea.ac.uk>
2013-03-01 13:57:05 +0000

    Added tests to the Barchart class, created a new toString method for the ColourMap class for outputting the colour array in tests

--- cs235/src/cs235/MS_Barchart.java
+++ cs235/src/cs235/MS_Barchart.java
@@ -4,20 +4,20 @@
  */
 package cs235;
 
-import java.awt.Rectangle;
-import org.jfree.chart.ChartFactory;
-import org.jfree.chart.ChartPanel;
-import org.jfree.chart.JFreeChart;
-import org.jfree.chart.plot.PlotOrientation;
-import org.jfree.data.category.DefaultCategoryDataset;
-import org.jfree.chart.renderer.category.BarRenderer; 
 import java.awt.Color; 
 import java.awt.Paint;
+import java.awt.Rectangle;
 import java.io.File;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.CategoryPlot;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.renderer.category.BarRenderer;
 import org.jfree.chart.renderer.category.CategoryItemRenderer;
+import org.jfree.data.category.DefaultCategoryDataset;
 
 /**
 *@author William Bray
@@ -28,18 +28,6 @@ import org.jfree.chart.renderer.category.CategoryItemRenderer;
 
 public class MS_Barchart extends MS_Chart{
     
-     /**
-     * constructor setting all class variables needed to create a chart
-     * @param db - the data for the chart
-     * @param xColumnPos - the column data for the x axis from the dataset
-     * @param yColPos - the column data for the y axis from the dataset
-     * @param title - chart title
-     * @param r - to display the chart in the windows native size
-     */
-    public MS_Barchart(MS_DataSet db,int xColumnPos, int yColPos, String title, Rectangle r, MS_ColourMap cm){
-        super(db, xColumnPos, yColPos, title, r, cm);   
-    }
-
     /**
      * Accessor method for returning the current colour map being used
      * @return MS_ColourMap super.getColourMap() 
@@ -72,6 +60,19 @@ public class MS_Barchart extends MS_Chart{
         super.setColourMap(cm);
         return true;
     }
+    
+     /**
+     * constructor setting all class variables needed to create a chart
+     * @param db - the data for the chart
+     * @param xColumnPos - the column data for the x axis from the dataset
+     * @param yColPos - the column data for the y axis from the dataset
+     * @param title - chart title
+     * @param r - to display the chart in the windows native size
+     */
+    public MS_Barchart(MS_DataSet db,int xColumnPos, int yColPos, String title, Rectangle r, MS_ColourMap cm){
+        super(db, xColumnPos, yColPos, title, r, cm);   
+    }
+    
     
     /**
      * A class which constructs a dataset which is usable for the creation
@@ -128,7 +129,7 @@ public class MS_Barchart extends MS_Chart{
         );
         
         final CategoryPlot plot = CHART.getCategoryPlot(); 
-        CategoryItemRenderer renderer = new CustomRenderer(); 
+        CategoryItemRenderer renderer = new MS_CustomRenderer(); 
         plot.setRenderer(renderer);
         return CHART;
     }
@@ -139,14 +140,14 @@ public class MS_Barchart extends MS_Chart{
      * A renderer specific for this type of chart. Sets the colours that will 
      * be used when displaying the chart.
      */
-    class CustomRenderer extends BarRenderer { 
+    class MS_CustomRenderer extends BarRenderer { 
         private Paint[] colors;
         
         /**
          * The constructor for the custom renderer, which will set the colours
          * of the bars
          */
-        public CustomRenderer(){ 
+        public MS_CustomRenderer(){ 
            MS_ColourMap mappedColours = getColourMap();
            this.colors = new Paint[] {
              mappedColours.getColour(0), mappedColours.getColour(1), mappedColours.getColour(2), 
@@ -181,7 +182,7 @@ public class MS_Barchart extends MS_Chart{
     //TESTING CODE BEYOND THIS POINT
     //TURN AROUND NOW!
     
-    
+    //This code will create a basic chart, testing the various methods 
     
     
     public static void main (String[] args){
@@ -205,7 +206,8 @@ public class MS_Barchart extends MS_Chart{
         
         // MS_Barchart(MS_DataSet db,int xColumnPos, int yColPos, String title, Rectangle r, MS_ColourMap cm){
         
-        container.add(new MS_Barchart(db,xColumn,yColumn,"title",view.getBounds(), cm));
+        MS_Barchart bc = new MS_Barchart(db,xColumn,yColumn,"title",view.getBounds(), cm);
+        container.add(bc);
         
         
         container.validate();
@@ -214,7 +216,32 @@ public class MS_Barchart extends MS_Chart{
         view.repaint();
         view.setVisible(true);
         
+        //Testing individual methods
+        System.out.println("///////////////BEGIN TESTING\\\\\\\\\\\\\\\\");
+        
+        //Colour map testing
+        System.out.println("Testing getColourMap");
+        System.out.println("Colour map data: " + bc.getColourMap().toString(bc.getColourMap()));
+        System.out.println("New colour map with different colours");
+        Color[] ca = {Color.BLACK, Color.RED, Color.DARK_GRAY, Color.LIGHT_GRAY, Color.WHITE};
+        MS_ColourMap testCM = new MS_ColourMap(ca);
+        bc.setColourMap(testCM);
+        System.out.println("New colour map data: " + bc.getColourMap().toString(bc.getColourMap()));
+        
+        //Title setting and getting test
+        System.out.println("Getting default title with getTitle");
+        System.out.println("Original title is: " + bc.getTitle());
+        System.out.println("Testing setChartTitle with 'New Title'");
+        bc.setChartTitle("New Title");
+        System.out.println("Checking title set with getTitle");
+        System.out.println("New title is: " + bc.getTitle());
+        
+        
+        
     }
+    
+
+            
     
 }
 
